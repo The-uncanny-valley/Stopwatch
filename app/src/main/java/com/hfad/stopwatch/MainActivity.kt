11c569d807b8c091f42fc15461de5efb9a46caf0
the-uncanny-valley <runcannyvalley@gmail.com>
2025-07-21 14:47:41 +0300

    Implement stopwatch functionality with start, pause, reset, and state preservation

--- app/src/main/java/com/hfad/stopwatch/MainActivity.kt
+++ app/src/main/java/com/hfad/stopwatch/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.hfad.stopwatch
 
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var stopwatch: Chronometer
+    var running = false
+    var offset: Long = 0 // базовое смещение
+
+    // строки для ключей, используемых с Bundle
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +29,57 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        stopwatch = findViewById<Chronometer>(R.id.stopwatch)
+
+        // восстановление предыдущего состояния
+        if (savedInstanceState != null) {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running) {
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            } else setBaseTime()
+        }
+
+        val startButton = findViewById<Button>(R.id.start_button)
+        startButton.setOnClickListener {
+            if (!running) {
+                setBaseTime()
+                stopwatch.start()
+                running = true
+            }
+        }
+
+        val pauseButton = findViewById<Button>(R.id.pause_button)
+        pauseButton.setOnClickListener {
+            if (running) {
+                saveOffset()
+                stopwatch.stop()
+                running = false
+            }
+        }
+
+        val resetButton = findViewById<Button>(R.id.reset_button)
+        resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime() // обнулить показания секундомера
+        }
+    }
+
+    // метод для сохранения свойств offset, running и stopwatch.base
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    private fun saveOffset() {
+        offset = SystemClock.elapsedRealtime() - stopwatch.base
+    }
+
+    private fun setBaseTime() {
+        stopwatch.base = SystemClock.elapsedRealtime() - offset
     }
 }
